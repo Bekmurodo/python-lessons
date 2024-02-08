5e329b40924a1aca5e95d7d25aa8ff324175e9c9
Bekmurodo <zsuhrob240@gmail.com>
2024-02-08 21:17:25 +0300

    Create Hello world!.py

---
+++ Hello world!.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 17:23:51 2024
+
+@author: Suhrob
+"""
+
+print('Salom Dunyo!');
+print("Assalom alaykum")
+print('Xayrli kech')
+print(2+4*2)
+print(19/5)
+print(2**4)
