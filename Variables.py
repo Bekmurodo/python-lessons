60c79b2e4a9603c56a14a66d01a08909b0f65897
Bekmurodo <zsuhrob240@gmail.com>
2024-02-09 20:26:19 +0300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 19:58:26 2024
+
+@author: Suhrob
+Theme: Variables
+"""
+#ism = "Abdulloh"
+#yosh = 21
+#print(ism)
+#print(yosh)
+
+#ism = "Abdulloh"
+#print(ism)
+#ism = "Muhammad"
+#print(ism)
+
+#a = 5
+#b = 7
+#c = (a + b)**2
+#print(c)
+
+firstCode = "Hello World!"
+print(firstCode) 
+
+xabar = "Salom hammaga"
+print(xabar)
+xabar = "Assalom Alaykum"
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
