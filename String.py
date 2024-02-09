3bfc5cf008a46a47da093bf33a462b2bac1f374e
Bekmurodo <zsuhrob240@gmail.com>
2024-02-09 20:26:23 +0300

    Create String.py

---
+++ String.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 18:00:55 2024
+
+@author: Suhrob
+Theme: Strings
+"""
+ism = "Suhrob"
+
+shahar = "Кукон"
+viloyat = 'Фаргона'
+
+matn = 'Men yangi telefon oldim ' 
+
+
+#ism = "Ahmad"
+#print("Mening ismim " + ism)
+
+ism = "Ahad"
+familiya ="Qayum"
+#print(ism + familiya)
+#print(ism + ' ' + familiya)
+
+ismSharif = f"{ism} {familiya}"
+#print(ismSharif)
+
+ism = "James"
+familiya = "Bond"
+#print(f"Salom, mening ismim {familiya}. {ism} {familiya}!")
+
+#print('Hello world!')
+#print('Hello \tWorld!')
+#print("Hello \nWorld!")
+
+ism = 'james'
+familiya = 'bond'
+ismSharif = f"{ism} {familiya}"
+#print(ismSharif.upper())
+#print(ismSharif.lower())
+#print(ismSharif.title())
+#print(ismSharif.capitalize())
+
+meva = "    olma    "
+#print(meva)
+#print("Men " + meva.lstrip() + " yaxshi ko'raman")
+#print("Men " + meva.rstrip() + " yaxshi ko'raman")
+#print("Men " + meva.strip() + " yaxshi ko'raman")
+#print("Men " + meva + " yaxshi ko'raman")
+
+#ism = input("Ismingiz nima?")
+#print("Assalomu alaykum " + ism)
+
+#ism = input("Ismingiz nima?\n>>>")
+#print("Assalom alaykum, " + ism.title())
+
+#Amaliyot
+kocha="Bog'bon"
+mahalla="Sog'bon"
+tuman="Bodomzor" 
+viloyat="Samarqand"
+manzil = f"{kocha} ko'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati"
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
+
+#kocha = input("Ko'changizni kiriting >>>")
+#mahalla = input("Mahallangizni kiriting >>>")
+#tuman = input("Tumaningizni kiriting >>>")
+#viloyat = input("Viloyatingizni kiriting >>>")
+#print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
