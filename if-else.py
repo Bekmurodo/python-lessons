962c555ecf437658387ff1813f84d94a27672680
Bekmurodo <zsuhrob240@gmail.com>
2024-02-10 21:21:45 +0300

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  6 18:48:58 2024
+
+@author: Suhrob
+Theme: If-else shartlari
+"""
+
+#avtolar = ["audi",'bmw','volvo','kia','hyundai']
+#for avto in avtolar:
+#    if avto == 'bmw':
+#        print(avto.upper())
+#    else:
+#         print(avto.title())
+         
+#ism = 'Ali'
+
+#ism = input("Ismingiz nima?\n>>>")
+#if ism.lower() != 'ali':
+#    print(f"Uzr, {ism.title()} biz Alini kutayapmiz.")
+#else:
+#    print("Salom, Ali")
+
+#javob = float(input("12x6 nechiga teng:"))
+#if javob != 72:
+#    print("Javob xato!")
+#else:
+#    print("Tog'ri topdingiz")
+
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh>=18:
+#    print("Xush kelibsiz")
+#else:
+#    print("Kirish mumkun emas!")
+
+#login = input("Yangi login kiriting:")
+#if len(login)<=5:
+#    print("Login 5 harfdan ko'proq bo'lishi shart!")
+    
+#yil = int(input("Tug'ulgan yilingizni kiriting:"))
+#if 2024-yil<18:
+#    print(f"Yoshingiz {2024-yil}da ekan.")
+#    print("Kirish mumkun emas!")
+#else:
+#    print("Xush kelibsiz!")
+    
+#yosh = int(input("Yoshingiz nechida?>>>"))
+#if yosh >65: print("Siz COVID-19 risk guruhida ekansiz")
+
+#x, y = 53, 50
+#print("x>y") if x>y else print("x<y")
+
+#Amaliyot
+#cars = ['toyota','mazda','hyundai','gm','kia']
+#for car in cars:
+#    if car != 'gm':
+#        print(car.title())
+#    else:
+#        print(car.upper())
+
+#admin = input("Loginingizni kiriting:")
+#if admin.lower == 'admin':
+#    print("Xush kelibsiz, Admin, Foydalanuvchilar ro'yxatini ko'rasizmi?")
+#else:
+#    print(f"Xush kelibsiz {admin.title()}")
+
+#b_son = int(input("Birinchi sonni kiriting:"))
+#i_son =  int(input("Ikkinchi sonni kiriting:"))  
+#if b_son == i_son:
+#    print("Sonlar teng")
+#random = int(input("Istalgan son kiriting:"))
+#if random > 0:
+#    print("Musbat son")
+#else:
+#    print('Manfiy son')
+
+#son = int(input("Biron son kiriting:"))
+#if son > 0:
+#    print(f"{son}ning ildizi {son ** 0.5} ga teng.")
+#else:
+#    print("Musbat son kiriting.")
+
+
+
+
+
+
+
+
