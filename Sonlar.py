87431849e85bcea226366c0ad4c0a4aefbaa04c0
Bekmurodo <zsuhrob240@gmail.com>
2024-02-11 19:37:44 +0300

    Create Sonlar.py

---
+++ Sonlar.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 19:36:00 2024
+
+@author: Suhrob
+Theme: Sonlar bilan ishlash
+"""
+#radius = 20
+#PI = 3.14159
+#diametr = 2*radius
+#print("Aylananing uzunligi=", PI*diametr)
+
+#ism = "Jobir"
+#yosh = 36
+#xabar = ism + " " + str(yosh) + ' yoshda'
+#print(xabar)
+
+#t_yil = input("Tugulgan yilingizni kiriting:")
+#t_yil = int(t_yil)
+#yosh = 2024 - t_yil
+#print(f"Siz {yosh} yoshdasiz.")
+
+#Amaliyot
+#son = int(input("Istalgan son kiriting:\n>>>"))
+#kvadrat = son**2
+#kub = son**3
+#print(f"{son} ning kvadrati {kvadrat} ga teng")
+#print(f"{son} ning kubi {kub} ga treng")
+
+#yosh = int(input("Yoshingiz nechida?"))
+#t_yil = 2024 - yosh
+#print(f"Siz {t_yil} da tug'ulgansiz")
+
+bir_son = float(input("Birinchi sonni kiriting:"))
+ikki_son = float(input("Ikkinchi sonni kiriting:"))
+print(f"{bir_son} + {ikki_son} = {bir_son + ikki_son}")
+print(f"{bir_son} - {ikki_son} = {bir_son - ikki_son}")
+print(f"{bir_son} * {ikki_son} = {bir_son * ikki_son}")
+print(f"{bir_son} / {ikki_son} = {bir_son / ikki_son}")
+
+
+
+
+
+
+
+
+
+
+
+
+
