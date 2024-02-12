efaf1028cda0f662490b4e34114a587a90fea704
Bekmurodo <zsuhrob240@gmail.com>
2024-02-12 21:17:00 +0300

    Create Print-function.py

---
+++ Print-function.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 17:51:28 2024
+
+@author: Suhrob
+"""
+#print(2+4*2)
+#print("To'qqizning kvadrati", 9**2, "ga teng")
+#print('3x3=',3*3)
+##print(16/4) #4.0
+##print(16//4) #4
+
+print('"Nexia",', "\"Tico\",", "'Damas' " "k'organlar qilar havas")
+#5 ning 4-chi darajasini toping 
+print(f'5 ning 4-chi darajasi {5**4}')
+#22ni 4ga bo'lganda qancha qoldiq qoladi
+print(22%4)
+print(125*125)# kvadratning yuzi
+print(125*4) #kvadratning perimetri
+print(3.14*(12/2)**2)
+print(6**2+7**2**(1/2))
+
+
+
+
