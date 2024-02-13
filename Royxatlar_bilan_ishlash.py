ce76a02d50f564a217966d9a0ddf232ea842b98d
Bekmurodo <zsuhrob240@gmail.com>
2024-02-13 20:01:12 +0300

    Create Royxatlar_bilan_ishlash.py

---
+++ Royxatlar_bilan_ishlash.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 19:21:17 2024
+
+@author: Suhrob
+Theme: Royxatlar bilan ishlash
+"""
+#cars = ['bmw', 'mercedes benz', 'volvo', 'genelar motors', 'toyota', 'corolla', 'tesla', 'audi']
+#narxlar = [24323,5600,6534,7895,17658,4686,1001]
+
+#toys = ('bus', 'car', 'bear', 'dino', 'snake', 'lizard')
+
+#Amaliyot
+countries = ['USA','UAE','UK','Canada','Russia','Tajikistan','Uzbekistan']
+#print(countries)
+#print(len(countries))
+#print(sorted(countries))
+#print(countries)
+#countries.reverse()
+#print(countries)
+#countries.sort()
+#print(countries)
+#countries.sort(reverse=True)
+#print(countries)
+
+sonlar = list(range(120,1200,2))
+#print(sonlar)
+#print(sum(sonlar))
+mini = min(sonlar)
+maxi = max(sonlar)
+natija = maxi - mini
+#print(natija)
+#print(len(sonlar))
+#print(sonlar[:])
+#print(sonlar[268:274])
+#print(sonlar[534:])
+
+taomlar = ['osh','manti','kabob','sho\'rva','gosht']
+nonushta = taomlar[:]
+nonushta.remove('osh')
+nonushta.remove('manti')
+nonushta.remove('kabob')
+nonushta.append('tuxm')
+nonushta.append('sut')
+nonushta.append('saryog\'')
+print(taomlar)
+print(nonushta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
