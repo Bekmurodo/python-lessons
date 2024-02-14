9a171970dd52372ad62808e43b9629767b00c086
Bekmurodo <zsuhrob240@gmail.com>
2024-02-14 20:57:12 +0300

    Update nesting.py

--- nesting.py
+++ nesting.py
@@ -119,17 +119,113 @@ hamkasblar = {
              'tillar':['python','php']}
     }
 
-for ism, info in hamkasblar.items():
-    print(f"\n{ism.title()} {info['familiya'].title()}, "
-          f"{info['tyil']}-yilda tug'ilgan. "
-          f"Ma'lumoti: {info['malumot']}. \n"
-          "Quyidagi dasturlash tillarini biladi:")
-    for til in info['tillar']:
-        print(til.upper())
+#for ism, info in hamkasblar.items():
+#    print(f"\n{ism.title()} {info['familiya'].title()}, "
+#          f"{info['tyil']}-yilda tug'ilgan. "
+#          f"Ma'lumoti: {info['malumot']}. \n"
+#          "Quyidagi dasturlash tillarini biladi:")
+#    for til in info['tillar']:
+#        print(til.upper())
 
 
+#Task 1
+shaxs_0 = {
+    'ism':'Abu Abdulloh Muhammad ibn Ismoil',
+    't_yil':810,
+    't_joy':'Buxoro',
+    'umr':63,
+    'asarlar': ['Al-jome as-sahih','Al-adab al-mufrad','At-tarix al-kabir','At-tarix as-sag\'ir']
+    }
 
+shaxs_1 = {
+    'ism':'Abdulla Qodiriy',
+    't_yil':1894,
+    't_joy':'Toshkent',
+    'umr':44,
+    'asarlar': ["O'tkan kunlar",'Mehrobdan Chayon', 'Obid ketmon']
+    }
 
+shaxs_2 = {
+    'ism':'Erkin Vohidov',
+    't_yil':1936,
+    't_joy':'Farg\'ona',
+    'umr':80,
+    'asarlar': ["Tong nafasi", "Qoshiqlarim sizga", "O'zbegim", "Qiziquvchan Matmusa"]
+    }
+
+shaxs_3 = {
+    'ism':'Alisher Navoiy',
+    't_yil':1441,
+    't_joy':'Hirot',
+    'umr':60,
+    'asarlar': ['Hamsa', 'Lison ut-Tayr', 'Mahbub Al-Qulub','Munojot']
+    }
+
+shaxslar = [shaxs_0, shaxs_1, shaxs_2, shaxs_3]
+#for shaxs in shaxslar:
+#    print(f"{shaxs['ism']} {shaxs['t_yil']}-yilda "
+#          f"{shaxs['t_joy']}da tavallud topgan. "
+#          f"{shaxs['umr']} yil umr ko'rgan.")
+
+#Task 2
+#for shaxs in shaxslar:
+#    ism = shaxs['ism']
+#    asarlar = shaxs['asarlar']
+#    print(f"\n{ism}ning mashxur asarlari: ")
+#    for asar in asarlar:
+#        print(asar)
+
+oila = {
+        'ali': ['terminator','rambo','titanic'],
+        'vali': ['tenet', 'inception','intersteller'],
+        'hasan': ['abdullajon','bomba','shaytanat'],
+        'husan': ['Mahallada duv-duv gap','jhon Wick']
+        }
+#for ism, kinolar in oila.items():
+#    print(f"\n{ism.title()}ning sevimli kinolari:")
+#    for kino in kinolar:
+#        print(f"{kino.capitalize()}")
+
+davlatlar = {
+    "o'zbekiston":{'poytaxt':"toshkent",
+                   'maydon':448978,
+                   'aholi':33_000_000,
+                   'pul birligi':"so'm"
+                   },
+    "rossiya":{'poytaxt':"moskva",
+                   'maydon':17_098_246,
+                   'aholi':144_000_000,
+                   'pul birligi':"rubl"
+                   },
+    "aqsh":{'poytaxt':"vashington",
+                   'maydon':9_631_418,
+                   'aholi':327_000_000,
+                   'pul birligi':"dollar"},
+    "malayziya":{'poytaxt':"kuala-lumpur",
+                   'maydon':329750,
+                   'aholi':25_000_000,
+                   'pul birligi':"rinngit"}
+    }
+
+for davlat, info in davlatlar.items():
+    if davlat.lower()=="aqsh":
+        davlat=davlat.upper()
+    else:
+        davlat = davlat.capitalize()
+#    print(f"\n{davlat}ning poytaxti {info['poytaxt'].title()} "
+#          f"\nHududi: {info['maydon']} kv.km "
+#         f"\nAholisi: {info['aholi']} "
+#          f"\nPul birligi: {info['pul birligi']}")
+    
+davlat = input("Davlat nomini kiriting: ").lower()
+if davlat in davlatlar:
+    info = davlatlar[davlat]
+    print(f"\n{davlat.title()}ning poytaxti {info['poytaxt'].title()} "
+          f"\nHududi: {info['maydon']} kv.km "
+          f"\nAholisi: {info['aholi']} "
+          f"\nPul birligi: {info['pul birligi']}")
+else:
+    print("Bizda bu davlat haqida malumot yo'q")
 
 
 
