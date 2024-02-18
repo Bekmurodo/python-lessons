2d8259e4eae6a304f8ff37087ec968122b86fbc1
Bekmurodo <zsuhrob240@gmail.com>
2024-02-18 20:42:19 +0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 20:23:42 2024
+
+@author: Suhrob
+Theme: Functions
+"""
+
+#def salom_ber():
+#    """Salom beruvchi funksiya"""
+#    print("Assalomu alaykum")
+#salom_ber()
+
+def salom_bergin(ism):
+    """Fodyalanuvchi ismini qabul qilib,
+    unga salom beruvchi funksiya"""
+    print(f"Assalomu alaykum, hurmatli {ism.title()}!")
+    
+salom_bergin('hasan')
+salom_bergin()
