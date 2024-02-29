6abf3f171729f319bb0607c54093d799a5ff2255
Bekmurodo <zsuhrob240@gmail.com>
2024-02-29 20:51:36 +0300

    Update sonTop_oyini.py

--- sonTop_oyini.py
+++ sonTop_oyini.py
@@ -19,6 +19,40 @@ def sontop(x=10):
             print("Men o'ylagan son bundan kichikroq. Yana harakat qilib ko'ring.")
         else:
             break
-    print(f"Tabriklaymiz. Siz {taxminlar} urunushda topdingiz.") 
+        return taxminlar
+    print(f"Tabriklaymiz. Siz {taxminlar} ta taxmin bilan topdingiz.") 
             
+def sontop_pc(x=10):
+    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman ")
+    quyi = 1
+    yuqori = x 
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        if quyi !=yuqori:
+            taxmin = r.randint(quyi, yuqori)
+        else:
+            taxmin = quyi
+        javob = input(f"Siz {taxmin} sonini o'yladingiz: to'gri (t),"
+                      f"men o'ylagan son bundan kattaroq (+), yoki kichikroq(-)".lower())
+        if javob == "-":
+            yuqori = taxmin - 1
+        elif javob == "+":
+            quyi = taxmin + 1
+        else:
+            break
+    print(f"Men {taxminlar} ta taxmin bilan topdim!")
+    
+def play(x=10):
+    yana = True
+    while yana:
+        taxminlar_user = sontop(x)
+        taxminlar_pc = sontop_pc(x)
         
+        if taxminlar_user>taxminlar_pc:
+            print(f"Men {taxminlar_pc} ta taxmin bilan topdim va yutdim!")
+        elif taxminlar_user<taxminlar_pc:
+            print(f"Siz {taxminlar_user} ta taxmin bilan topdingiz va yutdingiz!")
+        else:
+            print("Durrang!")
+        yana = int(input("Yana o'ynaysizmi? Ha(1)/Yo'q(1): "))
