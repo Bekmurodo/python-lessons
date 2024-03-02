0d34c56ba81e39c3809c9d9bfd403e44eb32481a
Bekmurodo <zsuhrob240@gmail.com>
2024-03-02 20:20:40 +0300

    Create moduls.py

---
+++ 23-functions-modules/moduls.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 19:02:03 2024
+
+@author: Suhrob
+Theme: Moduls in python
+"""
+#import avto_info_mod
+#avto1 = avto_info_mod.avto_info("GM", "Malibu", 'Qora', "avtomat", 2020,40000)
+#avto_info_mod.info_print(avto1)
+
+#import avto_info_mod as aim
+
+#avto1 = aim.avto_info("GM", "Malibu", 'Qora', "avtomat", 2020,40000)
+#avto_info_mod.info_print(avto1)
+
+#from avto_info_mod import avto_info, avto_kirit, info_print
+#avto1 = avto_info("GM", "Malibu", 'Qora', "avtomat", 2020,40000)
+#info_print(avto1)
+
+#from avto_info_mod import avto_info as ainfo, info_print as iprint
+#avto1 = ainfo("GM", "Malibu", 'Qora', "avtomat", 2020,40000)
+#iprint(avto1)
+
+from avto_info_mod import *
+avto1 = avto_info("GM", "Malibu", 'Qora', "avtomat", 2020,40000)
+info_print(avto1)
+
+
