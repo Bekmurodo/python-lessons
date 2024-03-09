90b97cc27e6f88c2caacd43d983f355874fc2589
Bekmurodo <zsuhrob240@gmail.com>
2024-03-09 22:21:45 +0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 19:52:03 2024
+
+@author: Suhrob
+Theme: Lists
+"""
+#mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+#narxlar = [12000, 18000, 10900, 22000, 25000, -32,36.6]
+#sonlar = ['bir', 'ikki', 3, 4, 5]
+#ismlar = []
+#print(mevalar[1])
+#print(mevalar[0].upper())
+
+#hayvonlar = ['it', 'mushuk', 'sigir', 'qo\'y', 'mushuk', 'quyon']
+#bozorlik = ["yog'", 'un', 'piyoz', "banan", 'gosht']
+
+#mahsulot = bozorlik.pop(2,)
+#print("Men " + mahsulot  + " sotib oldim")
+#print("Sotib olinmagan mahsulotlar ", bozorlik)
+
+#Amaliyot
+#ismlar = ["Ilhom", "Faxriddin", "Azamat", "Ehson"]
+#print("Salom ", ismlar[0], " bugun choyxona bormi?"),
+#print(ismlar[1], "choyxona boramizmi?")
+#print(ismlar[2], "chokadi?")
+#print("Football ko'rdingmi ", ismlar[-1])
+
+#sonlar = [2, 4, 76, 505, -43, 78.8, 1]
+#t_shaxslar = ["Amir Temur", "Muhammad s.a.v", "Imom Buxoriy"]
+#z_shaxslar = ["Ilon Mask", "Davronbek", "Aziz Rahimov", "Abdulloh domla"]
+
+#print("Men tarixiy shaxslardan", t_shaxslar.pop(-1), "bilan, zamonaviy shaxslardan esa ", z_shaxslar.pop(-2), "suhbat qilishni istar edim")
+friends = ["Murod", "Azamat", "Zoir", "John", "Alex"]
+mehmonlar = []
+mehmonlar.append(friends.pop(3))
+mehmonlar.append(friends.pop(-1))
+mehmonlar.append(friends.pop(0))
+print("\nKelgan mehmonlar: ", mehmonlar)
+
+
+
+
+
+
+
+
+
+
