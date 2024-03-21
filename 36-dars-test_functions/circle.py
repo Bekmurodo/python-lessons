460c2c9e0111fb7aaecb9feb6d9c3b9827202d4e
Bekmurodo <zsuhrob240@gmail.com>
2024-03-21 20:44:42 +0300

    Create circle.py

---
+++ 36-dars-test_functions/circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 16 19:27:19 2024
+
+@author: Suhrob
+"""
+def getAria(r,pi=3.14159):
+    return pi*(r**2)
+
+def getPerimeter(r,pi=3.14159):
+    return 2*pi*r
